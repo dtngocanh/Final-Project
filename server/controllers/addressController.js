226d922414d8370da57bc859b853387779bd86c2
unknown <anhngocthiduong@gmail.com>
2026-05-12 09:07:29 +0700

    fix(user): ship fee

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -1,83 +1,84 @@
 import { Province } from "../models/Address.js";
 import axios from "axios";
+import ErrorHandler from "../utils/errorHandler.js";
 
-export const addAddress = async (req, res) => {
-  try {
-    const {
-      userId,
-      firstName,
-      lastName,
-      email,
-      street,
-      city,
-      zipcode,
-      country,
-      phone,
-    } = req.body;
+// export const addAddress = async (req, res) => {
+//   try {
+//     const {
+//       userId,
+//       firstName,
+//       lastName,
+//       email,
+//       street,
+//       city,
+//       zipcode,
+//       country,
+//       phone,
+//     } = req.body;
 
-    if (
-      !userId ||
-      !firstName ||
-      !lastName ||
-      !email ||
-      !street ||
-      !city ||
-      !zipcode ||
-      !country ||
-      !phone
-    ) {
-      return res.status(400).json({
-        success: false,
-        message: "Please fill all fields",
-      });
-    }
+//     if (
+//       !userId ||
+//       !firstName ||
+//       !lastName ||
+//       !email ||
+//       !street ||
+//       !city ||
+//       !zipcode ||
+//       !country ||
+//       !phone
+//     ) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Please fill all fields",
+//       });
+//     }
 
-    const newAddress = new Address({
-      userId,
-      firstName,
-      lastName,
-      email,
-      street,
-      city,
-      zipcode,
-      country,
-      phone,
-    });
+//     const newAddress = new Address({
+//       userId,
+//       firstName,
+//       lastName,
+//       email,
+//       street,
+//       city,
+//       zipcode,
+//       country,
+//       phone,
+//     });
 
-    await newAddress.save();
+//     await newAddress.save();
 
-    res.status(201).json({
-      success: true,
-      message: "Address added successfully",
-      address: newAddress,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
+//     res.status(201).json({
+//       success: true,
+//       message: "Address added successfully",
+//       address: newAddress,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
 
-export const getUserAddress = async (req, res) => {
-  try {
-    const userId = req.body;
+// export const getUserAddress = async (req, res) => {
+//   try {
+//     const userId = req.body;
 
-    const addresses = await Address.find({ userId });
+//     const addresses = await Address.find({ userId });
 
-    res.status(200).json({
-      success: true,
-      addresses,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
+//     res.status(200).json({
+//       success: true,
+//       addresses,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
 
 export const getProvinces = async (req, res, next) => {
   try {
@@ -97,7 +98,7 @@ const ghnReq = axios.create({
   baseURL: process.env.GHN_BASE_URL,
   headers: {
     Token: process.env.GHN_TOKEN,
-    ShopId: process.env.GHN_SHOP_ID,
+    ShopId: Number(process.env.GHN_SHOP_ID),
   },
 });
 
@@ -114,20 +115,34 @@ const convertToUSD = async (vnd) => {
     return 0;
   }
 };
+const getWeight = (product) => {
+  if (product.weight) return product.weight;
+
+  switch (product.categoryName) {
+    case "Vegetables":
+      return 300; // Bag
+    case "Meats and Seafood":
+      return 500; // Bag
+    case "Fruits":
+      return 1000; // Kg
+    case "Packages":
+      return 200; //Box
+    default:
+      return 500;
+  }
+};
 
 export const calcFee = async (req, res, next) => {
   try {
     const { cartItems, to_district_id, to_ward_code } = req.body;
 
     if (!cartItems || !to_district_id || !to_ward_code) {
-      return res.status(400).json({
-        success: false,
-        message: "Missing required fields",
-      });
+      return next(new ErrorHandler("Missing required fields", 400));
     }
 
     const totalWeight = cartItems.reduce((sum, item) => {
-      return sum + item.quantity * 1000; // mỗi item = 1kg
+      const weightPerItem = getWeight(item.product);
+      return sum + item.quantity * weightPerItem;
     }, 0);
 
     const serviceRes = await ghnReq.post(
@@ -142,10 +157,9 @@ export const calcFee = async (req, res, next) => {
     const services = serviceRes.data?.data;
 
     if (!services || services.length === 0) {
-      return res.status(400).json({
-        success: false,
-        message: "No shipping service available",
-      });
+      return next(
+        new ErrorHandler("No shipping service available for this route", 400),
+      );
     }
 
     const service_id = services[0].service_id;
@@ -156,6 +170,9 @@ export const calcFee = async (req, res, next) => {
       to_district_id: Number(to_district_id),
       to_ward_code: String(to_ward_code),
       weight: totalWeight || 1000,
+      // length: 10,
+      // width: 10,
+      // height: 10,
       insurance_value: 500000,
     });
 
@@ -169,13 +186,9 @@ export const calcFee = async (req, res, next) => {
       feeVND,
       feeUSD,
       weight: totalWeight,
+      service_name: services[0].short_name,
     });
   } catch (error) {
-    console.error("GHN ERROR:", error.response?.data || error.message);
-
-    return res.status(500).json({
-      success: false,
-      message: error.response?.data?.message || "Internal Server Error",
-    });
+    next(error);
   }
 };
--- server/models/Address.js
+++ server/models/Address.js
@@ -5,22 +5,21 @@ const provinceSchema = new mongoose.Schema({
   provinceName: { type: String, required: true },
 });
 
-const districtSchema = new mongoose.Schema({
-  districtId: { type: Number, required: true, unique: true },
-  provinceId: { type: Number, required: true },
-  districtName: { type: String, required: true },
-});
+// const districtSchema = new mongoose.Schema({
+//   districtId: { type: Number, required: true, unique: true },
+//   provinceId: { type: Number, required: true },
+//   districtName: { type: String, required: true },
+// });
 
-const wardSchema = new mongoose.Schema({
-  wardCode: { type: String, required: true, unique: true },
-  districtId: { type: Number, required: true },
-  wardName: { type: String, required: true },
-});
+// const wardSchema = new mongoose.Schema({
+//   wardCode: { type: String, required: true, unique: true },
+//   districtId: { type: Number, required: true },
+//   wardName: { type: String, required: true },
+// });
 
 const Province =
   mongoose.models.province || mongoose.model("province", provinceSchema);
-const District = mongoose.model("district", districtSchema);
-const Ward = mongoose.model("ward", wardSchema);
+// const District = mongoose.model("district", districtSchema);
+// const Ward = mongoose.model("ward", wardSchema);
 
-export { Province, District, Ward };
-
+export { Province };
